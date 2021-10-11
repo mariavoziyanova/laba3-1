cdf781cd200827516f8203a763d9157a7c652746
mariavoziyanova <79759874+mariavoziyanova@users.noreply.github.com>
2021-10-11 13:29:26 +0300

    Add files via upload

--- test/tests.cpp
+++ test/tests.cpp
@@ -381,7 +381,7 @@ TEST(ShakerSort, sortSmall) {
 
 TEST(BubbleSort, bigArray) {
   RandomArrayGenerator gen;
-  Sequence<int> *main = gen.generate(1000);
+  Sequence<int> *main = gen.generate(new ArraySequence<int>(), 1000);
 
   ISorter<int> *sorter = new BubbleSort<int>();
   Sequence<int> *result = sorter->Sort(main, cmp);
@@ -395,7 +395,7 @@ TEST(BubbleSort, bigArray) {
 
 TEST(MergeSort, bigArray) {
   RandomArrayGenerator gen;
-  Sequence<int> *array = gen.generate(10000);
+  Sequence<int> *array = gen.generate(new ArraySequence<int>(), 10000);
 
   ISorter<int> *sorter = new MergeSort<int>();
   Sequence<int> *result = sorter->Sort(array, cmp);
@@ -409,7 +409,7 @@ TEST(MergeSort, bigArray) {
 
 TEST(QuickSort, bigArray) {
   RandomArrayGenerator gen;
-  Sequence<int> *array = gen.generate(10000);
+  Sequence<int> *array = gen.generate(new ArraySequence<int>(), 10000);
 
   ISorter<int> *sorter = new QuickSort<int>();
   Sequence<int> *result = sorter->Sort(array, cmp);
@@ -423,7 +423,7 @@ TEST(QuickSort, bigArray) {
 
 TEST(RandomizedQuickSort, bigArray) {
   RandomArrayGenerator gen;
-  Sequence<int> *array = gen.generate(10000);
+  Sequence<int> *array = gen.generate(new ArraySequence<int>(), 10000);
 
   ISorter<int> *sorter = new RandomizedQuickSort<int>();
   Sequence<int> *result = sorter->Sort(array, cmp);
@@ -437,7 +437,7 @@ TEST(RandomizedQuickSort, bigArray) {
 
 TEST(ShellSort, bigArray) {
   RandomArrayGenerator gen;
-  Sequence<int> *array = gen.generate(1000);
+  Sequence<int> *array = gen.generate(new ArraySequence<int>(), 1000);
 
   ISorter<int> *sorter = new ShellSort<int>();
   Sequence<int> *result = sorter->Sort(array, cmp);
@@ -519,11 +519,10 @@ TEST(LinkedList, add_elements) {
   ASSERT_EQ(22, list2.get(0));
   ASSERT_EQ(11, list2.get(1));
   // Подсписок
-  LinkedList<int> *subList = list2.getSubList(0, 0);
-  ASSERT_EQ(1, subList->getLength());
-  ASSERT_EQ(22, subList->getFirst());
-  ASSERT_EQ(22, subList->getLast());
-  delete subList;
+  LinkedList<int> subList = list2.getSubList(0, 0);
+  ASSERT_EQ(1, subList.getLength());
+  ASSERT_EQ(22, subList.getFirst());
+  ASSERT_EQ(22, subList.getLast());
   // Исключения при взятии подсписка
   try {
     list2.getSubList(-1, 0);
@@ -698,140 +697,8 @@ TEST(LinkedList, removeAt) {
   list.print();
 }
 
-TEST(ArraySequence, constructor) {
-  Sequence<int> *s = new ArraySequence<int>();
-  ASSERT_EQ(0, s->getLength());
-  for (int x = 1; x <= 10; x++) {
-    s->append(x);
-    ASSERT_EQ(x, s->getLength());
-    ASSERT_EQ(x, s->getLast());
-  }
-  delete s;
-}
 
-TEST(ArraySequence, ArraySequence_Append_Prepend_element) {
-  int data[] = {111, 222};
-  Sequence<int> *s = new ArraySequence<int>(data, _countof(data));  // Список целых чисел
-  ASSERT_EQ(2, s->getLength());
-  ASSERT_EQ(111, s->get(0));
-  ASSERT_EQ(222, s->get(1));
-  ASSERT_EQ(111, s->getFirst());
-  ASSERT_EQ(222, s->getLast());
-  s->append(333);
-  ASSERT_EQ(3, s->getLength());
-  ASSERT_EQ(333, s->get(2));
-  s->prepend(10);
-  ASSERT_EQ(4, s->getLength());
-  ASSERT_EQ(10, s->get(0));
-  ASSERT_EQ(111, s->get(1));
-  ASSERT_EQ(222, s->get(2));
-  ASSERT_EQ(333, s->get(3));
-  delete s;
-}
 
-// Создание второго динамического массива как копии первого
-TEST(ArraySequence, create_from_DynamicArray) {
-  int data[] = {123, 234, 345};
-  DynamicArray<int> da(data, _countof(data));     // Список целых чисел
-  Sequence<int> *s = new ArraySequence<int>(da);  // Список целых чисел
-  ASSERT_EQ(3, s->getLength());
-  ASSERT_EQ(data[0], s->get(0));
-  ASSERT_EQ(data[1], s->get(1));
-  ASSERT_EQ(data[2], s->get(2));
-  ASSERT_EQ(data[0], s->getFirst());
-  ASSERT_EQ(data[2], s->getLast());
-  s->append(333);
-  ASSERT_EQ(4, s->getLength());
-  ASSERT_EQ(333, s->getLast());
-  s->prepend(10);
-  ASSERT_EQ(5, s->getLength());
-  // 123, 234, 345
-  ASSERT_EQ(10, s->get(0));
-  ASSERT_EQ(data[0], s->get(1));
-  ASSERT_EQ(data[1], s->get(2));
-  ASSERT_EQ(data[2], s->get(3));
-  ASSERT_EQ(333, s->get(4));
-  Sequence<int> *sub = s->getSubsequence(1, 3);
-  ASSERT_EQ(3, sub->getLength());
-  ASSERT_EQ(123, sub->get(0));
-  ASSERT_EQ(234, sub->get(1));
-  ASSERT_EQ(345, sub->get(2));
-  // Вставим ещё один элемент посередине
-  sub->insertAt(4444, 1);
-  ASSERT_EQ(4, sub->getLength());
-  ASSERT_EQ(123, sub->get(0));
-  ASSERT_EQ(4444, sub->get(1));
-  ASSERT_EQ(234, sub->get(2));
-  ASSERT_EQ(345, sub->get(3));
-  delete s;
-  delete sub;
-}
-
-// Сцепление двух списков
-TEST(ArraySequence, concat) {
-  // Первая последовательность
-  int data1[] = {11, 22};
-  DynamicArray<int> da1(data1, _countof(data1));    // Список целых чисел
-  Sequence<int> *s1 = new ArraySequence<int>(da1);  // Список целых чисел
-  // Вторая последовательность
-  int data2[] = {33};
-  DynamicArray<int> da2(data2, _countof(data2));    // Список целых чисел
-  Sequence<int> *s2 = new ArraySequence<int>(da2);  // Список целых чисел
-  // Сцепляем два списка
-  Sequence<int> *res = s1->concat(s2);
-  ASSERT_EQ(3, res->getLength());
-  ASSERT_EQ(11, res->get(0));
-  ASSERT_EQ(22, res->get(1));
-  ASSERT_EQ(33, res->get(2));
-  delete s1;
-  delete s2;
-  delete res;
-}
-
-TEST(ArraySequence, findSubsequence) {
-  int data[] = {11, 22, 33, 44, 55};
-  DynamicArray<int> da(data, _countof(data));
-  Sequence<int> *main = new ArraySequence<int>(da);
-
-  {
-    int d[] = {11, 22};
-    Sequence<int> *sub1 = new ArraySequence<int>(DynamicArray<int>(d, _countof(d)));
-    ASSERT_EQ(0, main->findSubsequence(*sub1));
-    delete sub1;
-  }
-  {
-    int d[] = {22, 33};
-    Sequence<int> *sub = new ArraySequence<int>(DynamicArray<int>(d, _countof(d)));
-    ASSERT_EQ(1, main->findSubsequence(*sub));
-    delete sub;
-  }
-  {
-    int d[] = {33, 44, 55};
-    Sequence<int> *sub = new ArraySequence<int>(DynamicArray<int>(d, _countof(d)));
-    ASSERT_EQ(2, main->findSubsequence(*sub));
-    delete sub;
-  }
-  {
-    int d[] = {44, 55};
-    Sequence<int> *sub = new ArraySequence<int>(DynamicArray<int>(d, _countof(d)));
-    ASSERT_EQ(3, main->findSubsequence(*sub));
-    delete sub;
-  }
-  {
-    int d[] = {55};
-    Sequence<int> *sub = new ArraySequence<int>(DynamicArray<int>(d, _countof(d)));
-    ASSERT_EQ(4, main->findSubsequence(*sub));
-    delete sub;
-  }
-  {
-    int d[] = {33, 55};
-    Sequence<int> *sub = new ArraySequence<int>(DynamicArray<int>(d, _countof(d)));
-    ASSERT_EQ(-1, main->findSubsequence(*sub));
-    delete sub;
-  }
-
-  delete main;
-}
 
 TEST(LinkedListSequence, findSubsequence) {
   int data[] = {11, 22, 33, 44, 55};
